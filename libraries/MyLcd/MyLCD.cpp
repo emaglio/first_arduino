0fe05f45672ab3afc01beaddc7e52ff5f33d9b62
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-03-01 14:29:18 +1100

    Fixed issue with first set up of the display - go ahead with setCursor #2

--- libraries/MyLcd/MyLCD.cpp
+++ libraries/MyLcd/MyLCD.cpp
@@ -17,29 +17,62 @@ void MyLCD::init(uint8_t rs, uint8_t enable,
 	_data_pins[2] = d6;
 	_data_pins[3] = d7;
 
-//	begin(16,1);
+	begin(16,2,58);
 }
 
-void MyLCD::begin(uint8_t cols, uint8_t lines){
-//	setup digital as OUTPUT
+void MyLCD::begin(uint8_t cols, uint8_t lines, uint8_t font){
+	//setup digital as OUTPUT
 	pinMode(_rs_pin, OUTPUT);
+	pinMode(_enable_pin, OUTPUT);
 
 	for(int i = 0; i < 4; i++){
 		pinMode(_data_pins[i], OUTPUT);
 	}
 
-//	set low rs and enable to send the instructions
+	//default 4 bits, 1 line and 5x8 dots as font
+	_displayfunction = LCD_4BITS | LCD_1LINE | LCD_5x8DOTS;
+
+	if(lines == 2){
+		_displayfunction |= LCD_2LINE;
+	}
+
+	if(font == 511){
+		_displayfunction |= LCD_5x11DOTS;
+	}
+
+	//set low rs and enable to send the instructions
 	delayMicroseconds(50000);
 	digitalWrite(_rs_pin, LOW);
 	digitalWrite(_enable_pin, LOW);
 
-//	send twice to set the databus with 4bits
-//	because DB3 and DB2 are not connected so
-//	they will LOW which means 1 line and font 5x8 dots
-	write4bits(0x02);
+	//trying to force the 8bits (apparently necessary)
+	//3 times
+	write4bits(0x03);
 	delayMicroseconds(4500);
 
+	write4bits(0x03);
+	delayMicroseconds(4500);
+
+	write4bits(0x03);
+	delayMicroseconds(150);
+
+	//finally set 4bits
 	write4bits(0x02);
+
+	//Set bits, lines and font
+	command(LCD_FUNCTIONSET | _displayfunction);
+
+	//turn the display ON, no blinking, no cursor
+	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+	display();
+
+	//clean display
+	clean();
+
+	// Initialize to default text direction (for romance languages)
+	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+	// set the entry mode
+	command(LCD_ENTRYMODESET | _displaymode);
 }
 
 void MyLCD::write4bits(uint8_t value){
@@ -65,6 +98,7 @@ inline size_t MyLCD::write(uint8_t value) {
 }
 
 // write either command or data
+// needs to send the 4btis twice
 void MyLCD::send(uint8_t value, uint8_t mode) {
   digitalWrite(_rs_pin, mode);
 
@@ -72,14 +106,17 @@ void MyLCD::send(uint8_t value, uint8_t mode) {
   write4bits(value);
 }
 
-
 void MyLCD::command(uint8_t value){
 	send(value, LOW);
 }
 
 void MyLCD::clean(){
-	command(LCD_CLEAN);
+	command(LCD_CLEARDISPLAY);
 	delayMicroseconds(2000);
+}
+
+void MyLCD::display(){
+	command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
 
 
--- libraries/MyLcd/MyLCD.h
+++ libraries/MyLcd/MyLCD.h
@@ -4,8 +4,38 @@
 #include <inttypes.h>
 #include "Print.h"
 
+// commands
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
+
 //flag for function set
 #define LCD_CLEAN 0x00
+#define LCD_4BITS 0x00
+#define LCD_1LINE 0x00
+#define LCD_2LINE 0x08
+#define LCD_5x8DOTS 0x00
+#define LCD_5x11DOTS 0x04
+
+//flag for display ON/OFF
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
 
 class MyLCD : public Print{
 public:
@@ -14,9 +44,14 @@ public:
 	void init(uint8_t rs, uint8_t enable,
 		uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 
-	void begin(uint8_t cols, uint8_t lines);
-	void command(uint8_t value);
+	void begin(uint8_t cols, uint8_t lines, uint8_t font);
+
 	void clean();
+	void home();
+
+	void display();
+
+	void command(uint8_t value);
 	virtual size_t write(uint8_t);
 	using Print::write;
 private:
@@ -29,6 +64,8 @@ private:
 	uint8_t _enable_pin;
 	uint8_t _data_pins[4];
 
-	uint8_t _display_function;
+	uint8_t _displayfunction;
+	uint8_t _displaycontrol;
+	uint8_t _displaymode;
 };
 #endif
