7e403268c40df52919997a5b4b4cc4daf309ec77
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-03-16 16:12:36 +1100

    Added interrupts and some code for testing them #4

--- first_arduino.cpp
+++ first_arduino.cpp
@@ -8,16 +8,37 @@
 float tempVal;
 float lightVal;
 
+//these must be volatile to be used in the interrupt
+volatile float temp;
+volatile float light;
+
+const uint8_t ok_pin = 3;
+const uint8_t scroll_pin = 4;
+
+//libraries
 TempAndLight sensors(0,1);
-//LiquidCrystal lcd(11,12,5,4,3,2);
-
-MyLCD lcd(11,12,5,4,3,2);
-
+MyLCD lcd(11,12,7,6,5,4);
 
 void setup() {
 	Serial.begin(115200);
+
 	//columns, rows, font (58 -> 5x8 or 511 -> 5x11)
 	lcd.begin(16,2,58);
+
+	//using internal pullup resistor
+	pinMode(ok_pin, INPUT_PULLUP);
+	pinMode(scroll_pin, INPUT_PULLUP);
+	//set up the interrupt pin, what to call and when to call it
+	attachInterrupt(digitalPinToInterrupt(ok_pin), ok, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(scroll_pin), scroll, CHANGE);
+
+	//Start-up with something in the display
+	lcd.setCursor(2,0);
+	lcd.print("Welcome to:");
+	lcd.setCursor(1,1);
+	lcd.print("First Arduino");
+	delay(2000);
+	lcd.clean();
 }
 
 void loop() {
@@ -44,4 +65,16 @@ void loop() {
 //	Serial.println("Write temp");
 //	lcd.print(tempVal);
 //	delay(1000);
+}
+
+void ok(){
+	lcd.clean();
+	lcd.setCursor(0,0);
+	lcd.print("OK");
+}
+
+void scroll (){
+	lcd.clean();
+	lcd.setCursor(0,0);
+	lcd.print("scroll");
 }
