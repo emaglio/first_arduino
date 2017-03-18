ddbc257390384082e3a32e4e16eac7af567ff14d
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-03-18 14:29:01 +1100

    Added array of char for menu and introduction loop #3

--- first_arduino.cpp
+++ first_arduino.cpp
@@ -9,8 +9,11 @@ float tempVal;
 float lightVal;
 
 //these must be volatile to be used in the interrupt
-volatile float temp;
-volatile float light;
+volatile int ok_counter = 0;
+volatile int scroll_counter = 0;
+//first OK pressed to start the menu
+volatile int start_menu = 0;
+
 
 //button pins
 const uint8_t ok_pin = 3;
@@ -19,12 +22,19 @@ const uint8_t scroll_pin = 2;
 //buzzer pin
 const uint8_t buzzer_pin = 13;
 
+//Menu
+const char* level_1[2] {"Sensors", "RBG LED"};
+const char* level_1_1[3] {"Temperature", "Light", "<- BACK"};
+const char* level_1_2[5] {"OFF", "Blue", "Green", "Red", "<- BACK"};
+int level_1_counter = 0;
+int level_1_1_counter = 0;
+int level_1_2_counter = 0;
+
 //libraries
 TempAndLight sensors(0,1);
 MyLCD lcd(11,12,7,6,5,4);
 
 //Event functions
-
 void buzzer(){
 	tone(buzzer_pin, 1000);
 	delay(500);
@@ -32,17 +42,13 @@ void buzzer(){
 }
 
 void ok(){
-	lcd.clean();
-	lcd.setCursor(0,0);
-	lcd.print("OK");
 	Serial.println("ok");
+	ok_counter += 1;
 }
 
 void scroll (){
-	lcd.clean();
-	lcd.setCursor(0,0);
-	lcd.print("scroll");
 	Serial.println("scroll");
+	scroll_counter += 1;
 }
 
 void setup() {
@@ -55,8 +61,8 @@ void setup() {
 	pinMode(ok_pin, INPUT_PULLUP);
 	pinMode(scroll_pin, INPUT_PULLUP);
 	//set up the interrupt pin, what to call and when to call it
-	attachInterrupt(digitalPinToInterrupt(ok_pin), ok, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(scroll_pin), scroll, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(ok_pin), ok, RISING);
+	attachInterrupt(digitalPinToInterrupt(scroll_pin), scroll, RISING);
 
 	//buzzer set up
 	pinMode(buzzer_pin, OUTPUT);
@@ -68,14 +74,25 @@ void setup() {
 	lcd.print("First Arduino");
 	delay(4000);
 	lcd.clean();
+	delay(1000);
 }
 
 void loop() {
-	delay(3000);
-	lcd.setCursor(0,0);
-	lcd.print("Trailblazer");
-	lcd.setCursor(6,1);
-	lcd.print("PowerUser");
+	if(start_menu == 0 and ok_counter == 0){
+		lcd.setCursor(0,0);
+		lcd.print("Click OK to");
+		lcd.setCursor(0,1);
+		lcd.print("continue...");
+	}else{
+		while(true){
+			//Here all the code for the menu
+			lcd.clean();
+			lcd.setCursor(0,0);
+			lcd.print("Menu");
+			delay(250);
+		}
+	}
+
 
 //	lcd.print("Hello world!");
 //	Serial.println("Check Hello World");
