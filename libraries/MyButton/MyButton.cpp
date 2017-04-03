444ac0172664497c359f8a0c6dbeda3b8903e0bd
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-04-03 18:54:16 +1000

    Basic code added for MyButton library #3

--- libraries/MyButton/MyButton.cpp
+++ libraries/MyButton/MyButton.cpp
@@ -5,16 +5,83 @@
  *      Author: emaglio
  */
 
-#include "MyLCD.h"
+#include "MyButton.h"
 #include "Arduino.h"
 
+MyButton::MyButton(uint8_t pin){
+	init(pin);
+}
+
+void MyButton::init(uint8_t pin){
+	_button_pin = pin;
+	_event = 0;
+
+	begin(pin);
+}
 //call this to set up the button pin
-void MyButton::Begin(uint8_t pin){
+void MyButton::begin(uint8_t pin){
 	 pinMode(pin, INPUT);
 	 digitalWrite(pin, HIGH);
 }
 
-int MyButton::CheckButton(){
+int MyButton::checkButton(){
+	readButton();
+	getButtonEvents();
+	result = checkResult();
 
-	return _result;
+	return result;
+}
+
+void MyButton::readButton(){
+	// read button
+	if(digitalRead(_button_pin) == HIGH){
+		_buttonVal = true;
+	}else{
+		_buttonVal = false;
+	}
+}
+
+void MyButton::getButtonEvents(){
+	// button pressed
+	if(_buttonVal == true && _previousVal == false && (millis()-_timeUp) > _debounce){
+		//set timing variables
+		if(_event >= 1 && _buttonClicked == true){
+			_timeBetweenDowns = millis() - _timeUp;
+		}
+		_timeDown = millis();
+
+		//reset variables
+		result = 0;
+		_buttonClicked = false;
+		_previousVal = true;
+
+	// button released
+	}else if(_buttonVal == false && _previousVal == true && (millis()-_timeDown) > _debounce){
+		_timeUp = millis();
+		_timePressed = _timeUp - _timeDown;
+		_event += 1;
+		_buttonClicked = true;
+	}
+}
+
+void MyButton::checkResult(){
+	//check results only if the button has been successfully clicked
+	if(_buttonClicked == true){
+		// double click
+		if(_event >= 1 && _timeBetweenDowns < 250){
+			result = 2;
+		}
+		// hold
+		if(_timePressed > 1000 && _timePressed < 3000){
+			result = 3;
+		}
+		// long hold
+		if(_timePressed > 3000){
+			result = 3;
+		}
+		// normal click
+		if(result == 0){
+			result = 1;
+		}
+	}
 }
--- libraries/MyButton/MyButton.h
+++ libraries/MyButton/MyButton.h
@@ -5,25 +5,42 @@
  *      Author: emaglio
  */
 
+//Button has been pressed when HIGH is detected
+
+#include <inttypes.h>
+
 #ifndef MyButton_h
 #define MyButton_h
 
 class MyButton {
 public:
-	void Begin(uint8_t pin);
-	int CheckButton();
+	MyButton(uint8_t pin);
+	void init(uint8_t pin);
+	void begin(uint8_t pin);
+	int checkButton();
+	void readButton();
+	void getButtonEvents();
+	void checkResult();
+
 
 private:
 	//button time variables
+	uint8_t _button_pin = 0;
 	int _debounce = 20;			// ms debounce period to prevent flickering when pressing or releasing the button
 	int _DCgap = 250;			// ms hold period: how long to wait for press+hold event
 	int _holdTime = 1000;		// ms long hold period: how long to wait for press+hold event
 	int _longHoldTime = 3000;	// ms long hold period: how long to wait for press+hold event
 
 	//button variables
-	int _result = 0;	//1 click ones, 2 double click, 3 hold time, 4 long hold time
-	boolean currentVal = true;  //true = HIGH, false = LOW
-	boolean previousVal = true; //true = HIGH, false = LOW
+	int result = 0;					//1 click ones, 2 double click, 3 hold and 4 long hold
+	bool _buttonVal = false;  		//true = HIGH, false = LOW
+	bool _previousVal = false; 		//true = HIGH, false = LOW
+	long _timeUp = 0; 				//time when button has been release
+	long _timeDown = 0; 			//time when button has been pressed
+	int _timePressed = 0; 			//time button pressed down
+	long _timeBetweenDowns = 0;		//time between 2 consecutive button switch
+	int _event = 0; 				//number of times the button is pressed successfully
+	bool _buttonClicked = false;	//flag to confirm the button has been pressed correctly
 
 };
 
